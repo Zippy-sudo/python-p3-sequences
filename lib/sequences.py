007ab2cc4377da51e0699dc50b70f237fa284e73
Nehemiah Madahana <nehemiahmadahana230@gmail.com>
2024-11-27 23:08:38 +0300

    First Trial

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib = []
+
+    if length < 1 :
+        pass
+    elif length == 1 :
+        fib.append(0)
+    elif length == 2 :
+        fib.append(0)
+        fib.append(1)
+    else :
+        fib.append(0)
+        fib.append(1)
+
+        while len(fib) < length:
+            fib.append(fib[-2] + fib[-1])
+
+    return print(fib)
